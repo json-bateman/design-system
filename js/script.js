e821ff5a4f96e6667738d817cd822d4d3fd5d763
jacktrusler <jtrus93@gmail.com>
2025-03-23 20:42:46 -0400

    adding tablist, mobile styles

--- js/script.js
+++ js/script.js
@@ -10,3 +10,29 @@ close.addEventListener("click", () => {
   navbar.setAttribute('data-visible', "false");
 })
 
+/* Handle tab groups */
+document.querySelectorAll('.tab-group').forEach(group => {
+  const tabs = group.querySelectorAll('[role="tab"]');
+  const panels = group.querySelectorAll('.tab-panel');
+
+  tabs.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.setAttribute('aria-selected', 'false'));
+      panels.forEach(a => a.classList.add('hidden'));
+
+      tab.setAttribute('aria-selected', 'true');
+      panels[i].classList.remove('hidden');
+      tabs[i].classList.add('tab-border')
+
+      tabs.forEach((tab) => {
+        if (tab.getAttribute('aria-selected') === 'true') {
+          tab.classList.add('tab-selected');
+        } else {
+          tab.classList.remove('tab-selected');
+        }
+      })
+    });
+  });
+
+});
+
